6d7fbb037123386996ffcc19da4b91d7bb8d3f5f
Jaimes DE SOUSA GOMES <jaimes.dsg@gmail.com>
2026-01-07 16:34:08 +0000

    Removed not field table, do the exercice and remove unused folder

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -4,8 +4,7 @@ select
     paymentmethod as payment_method,
     status,
 
-    -- amount is stored in cents, convert it to dollars
     amount / 100 as amount,
     created as created_at
 
-from dbt-tutorial.stripe.payment 
+from {{ source('stripe', 'payment') }}  -- dbt-tutorial.stripe.payment 
