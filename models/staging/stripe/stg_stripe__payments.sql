ecdd15f59c97283892be680fd2c5dba428a2ea15
Jaimes DE SOUSA GOMES <jaimes.dsg@gmail.com>
2026-01-08 08:59:02 +0000

    Updated stripe sql and added custom test for it

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -2,9 +2,9 @@ select
     id as payment_id,
     orderid as order_id,
     paymentmethod as payment_method,
-    status,
-
-    amount / 100 as amount,
-    created as created_at
+    status as payment_status,
+    amount,
+    created,
+    _batched_at
 
 from {{ source('stripe', 'payment') }}  -- dbt-tutorial.stripe.payment 
